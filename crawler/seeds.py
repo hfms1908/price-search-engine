cd0dee48f63c0d4d9d92862619e4e2cb2c633ff6
hfms1908 <hfms1908@gmail.com>
2026-09-22 20:31:50 -0300

    seed.py: substituídas urls e domínios por dicionário de sources.

--- crawler/seeds.py
+++ crawler/seeds.py
@@ -1,29 +1,72 @@
-SEED_URLS = [
-    # "https://www.magazineluiza.com.br/busca/smartphone/",
-    # "https://www.kabum.com.br/celular-smartphone/smartphones",
-    # "https://site.fastshop.com.br/celular-tablet-e-smartwatch/celular-e-smartphone",
-    # "https://www.americanas.com.br/celulares-e-smartphones",
-    # "https://www.mercadolivre.com.br/c/celulares-e-telefones",
-    # "https://www.extra.com.br/c/telefones-e-celulares/smartphones?filtro=c38_c326",
-    "https://www.casasbahia.com.br/c/telefones-e-celulares?filtro=categoria-c38_c326",
-    # "https://www.amazon.com.br/gp/browse.html?node=16243890011&ref_=nav_em__wireless_smartphones_0_2_16_3",
-    # "https://www.samsung.com/br/smartphones/all-smartphones/",
-    # "https://www.pontofrio.com.br/c/telefones-e-celulares/smartphones?filtro=categoria-c38_c326",
-    # "https://www.carrefour.com.br/categoria/celulares-smartphones-e-smartwatches",
-    # "https://www.oficinadosbits.com.br/categoria/celulares-e-comunicacao-smartphone/",
-]
-
-ALLOWED_DOMAINS = {
-    "magazineluiza.com.br",
-    "kabum.com.br",
-    "fastshop.com.br",
-    "americanas.com.br",
-    "mercadolivre.com.br",
-    "extra.com.br",
-    "casasbahia.com.br",
-    "amazon.com.br",
-    "samsung.com",
-    "pontofrio.com.br",
-    "carrefour.com.br",
-    "oficinadosbits.com.br",
+SOURCES = {
+    "magazineluiza": {
+        "seed": "https://www.magazineluiza.com.br/busca/smartphone/",
+        domain: "magazineluiza.com.br",
+    },
+    "kabum": {
+        "seed": "https://www.kabum.com.br/celular-smartphone/smartphones",
+        "domain": "kabum.com.br",
+    },
+    "fastshop": {
+        "seed": (
+            "https://site.fastshop.com.br/"
+            "celular-tablet-e-smartwatch/celular-e-smartphone"
+        ),
+        "domain": "fastshop.com.br",
+    },
+    "americanas": {
+        "seed": "https://www.americanas.com.br/celulares-e-smartphones",
+        "domain": "americanas.com.br",
+    },
+    "mercadolivre": {
+        "seed": "https://www.mercadolivre.com.br/c/celulares-e-telefones",
+        "domain": "mercadolivre.com.br",
+    },
+    "extra": {
+        "seed": (
+            "https://www.extra.com.br/"
+            "c/telefones-e-celulares/smartphones?filtro=c38_c326"
+        ),
+        "domain": "extra.com.br",
+    },
+    "casasbahia": {
+        "seed": (
+            "https://www.casasbahia.com.br/"
+            "c/telefones-e-celulares?filtro=categoria-c38_c326"
+        ),
+        "domain": "casasbahia.com.br",
+    },
+    "amazon": {
+        "seed": (
+            "https://www.amazon.com.br/"
+            "gp/browse.html?node=16243890011"
+            "&ref_=nav_em__wireless_smartphones_0_2_16_3"
+        ),
+        "domain": "amazon.com.br",
+    },
+    "samsung": {
+        "seed": "https://www.samsung.com/br/smartphones/all-smartphones/",
+        "domain": "samsung.com",
+    },
+    "pontofrio": {
+        "seed": (
+            "https://www.pontofrio.com.br/"
+            "c/telefones-e-celulares/smartphones?filtro=categoria-c38_c326"
+        ),
+        "domain": "pontofrio.com.br",
+    },
+    "carrefour": {
+        "seed": (
+            "https://www.carrefour.com.br/"
+            "categoria/celulares-smartphones-e-smartwatches"
+        ),
+        "domain": "carrefour.com.br",
+    },
+    "oficinadosbits": {
+        "seed": (
+            "https://www.oficinadosbits.com.br/"
+            "categoria/celulares-e-comunicacao-smartphone/"
+        ),
+        "domain": "oficinadosbits.com.br",
+    },
 }
